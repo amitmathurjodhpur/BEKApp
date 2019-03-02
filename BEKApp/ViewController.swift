76c4a543b7264b5b9fbb4a23f70668a5e513c2a8
Bhavik <Bhavik@bhaviks-mac.local>
2019-03-02 10:36:55 +0530

    Initial Commit

---
+++ BEKApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BEKApp
+//
+//  Created by Bhavik on 02/03/19.
+//  Copyright © 2019 Bhavik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
