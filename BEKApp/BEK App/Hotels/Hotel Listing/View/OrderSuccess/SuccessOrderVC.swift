74bea8659d485b2c75e6b99242a26b3284a857d4
amit <amathurn@in.ibm.com>
2019-03-20 21:19:14 +0530

    Commit other files.

---
+++ BEKApp/BEK App/Hotels/Hotel Listing/View/OrderSuccess/SuccessOrderVC.swift
@@ -0,0 +1,40 @@
+//
+//  SuccessOrderVC.swift
+//  BEKApp
+//
+//  Created by Bhavik on 05/03/19.
+//  Copyright © 2019 Bhavik. All rights reserved.
+//
+
+import UIKit
+
+class SuccessOrderVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func nextTask(_ sender: UIButton) {
+//        let dashboard = DashboardVC.instantiate(fromAppStoryboard: .Dashboard)
+//        self.navigationController?.pushViewController(dashboard, animated: false)
+//        self.navigationController?.popToRootViewController(animated: false)
+        
+        var i = 0
+        if let navControllers = self.navigationController?.viewControllers {
+            for vc in navControllers {
+                if type(of: vc) == HotelListingVC.self {
+                    self.navigationController?.popToViewController(vc, animated: true)
+                    return
+                }
+                i += 1
+            }
+        }
+    }
+}
